9422387acccd5622b493340d1dac4712077d6221
«ankr0710» <annakrapivnaya@yandex.ru>
2025-10-29 21:45:38 +0500

    new

---
+++ script.js
@@ -0,0 +1,48 @@
+const button = document.getElementById("button")
+const menu = document.getElementById("menu")
+const report = document.getElementById("report")
+
+
+const menuItems =[
+    {
+        id:1,
+        title:"All"
+    },
+    {
+        id:2,
+        title:"Ideas"
+    },
+    {
+        id:3,
+        title:"Personal"
+    },
+    {
+        id:4,
+        title:"Work"
+    },
+    {
+        id:5,
+        title:"Shopping list"
+    }
+]
+
+const report =[
+    {
+        id:1,
+        title:"Сдать отчет",
+        date:"19.10",
+        tag:4
+    },
+    {
+        id:2,
+        title:"Список покупок"б
+        date:"20.10"
+        tag:5
+    },
+    {
+        id:3,
+        title:"Список дел"
+        date:"21.10",
+        tag:3
+    }
+]
