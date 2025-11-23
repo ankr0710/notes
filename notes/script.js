f9db6a9ebf79d2595ebec8a357a628f8613eaf55
«ankr0710» <annakrapivnaya@yandex.ru>
2025-11-23 22:41:32 +0500

    new

---
+++ notes/script.js
@@ -0,0 +1,52 @@
+const button = document.getElementById("button")
+const menu = document.getElementById("menu")
+const report = document.getElementById("report")
+
+
+const menuItems =[
+    {
+        id:1,
+        title:"All"
+    },
+    {
+        id:2,
+        title:"Ideas"
+    },
+    {
+        id:3,
+        title:"Personal"
+    },
+    {
+        id:4,
+        title:"Work"
+    },
+    {
+        id:5,
+        title:"Shopping list"
+    }
+]
+
+const note =[
+    {
+        id:1,
+        title:"Сдать отчет",
+        date:"19.10",
+        tag:4
+    },
+    {
+        id:2,
+        title:"Список покупок",
+        date:"19.10",
+        tag:5
+    },
+    {
+        id:3,
+        title:"Список дел",
+        date:"21.10",
+        tag:3
+    }
+]
+
+function createnote(note){
+    
+}
